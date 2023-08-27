775a873616f5f5d61876c7e968188f45566d5d52
ehmaddd <ehmaddd@gmail.com>
2023-08-27 23:01:40 +0500

    Add controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'greetings/random'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/random_greeting', to: 'greetings#random'
 end
